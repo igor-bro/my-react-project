bf2db49eb85dd86100f755016a52e2259c215e18
Igor Kushneruk <71212927+igor-bro@users.noreply.github.com>
2024-02-05 12:28:46 +0100

    firstprops

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,13 @@
-// src/Product.jsx
+// src/components/Product.jsx
 
-export const Product = () => {
-  return (
-    <div>
-      <h2>Tacos</h2>
-      <p>Price: 999 credits</p>
-    </div>
-  );
-};
+export const Product = ({
+  name,
+  imgUrl = '<https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder>',
+  price,
+}) => (
+  <div>
+    <h2>{name}</h2>
+    <img src={imgUrl} alt={name} width="480" />
+    <p>Price: {price} credits</p>
+  </div>
+);
